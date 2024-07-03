943af10c7a1b50cac2a0f4d1fd65a4102e97bce9
Suelem Macedo <suelemmacedo_@hotmail.com>
2024-07-03 19:45:20 -0300

    Finalizando o Header com estilização completa

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,31 @@
+import styled from "styled-components";
+
+const HeaderStyle = styled.header`
+  img {
+    width: Fixed(44px);
+    height: 35px;
+    top: 12.08px;
+    left: 32px;
+    gap: 10px;
+    opacity: 0px;
+
+  }
+
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  height: 60px;
+  background: #FFFFFF;
+  padding: 0 32px;
+  box-shadow: 0px 2px 8px 0px #00000033;
+`
+
+
 function Header() {
   return (
-    <img src="src/assets/Header.png" alt="" />
+    <HeaderStyle>
+      <img src="src/assets/Header.png" alt="Logo" />
+    </HeaderStyle>
   )
 
 }
